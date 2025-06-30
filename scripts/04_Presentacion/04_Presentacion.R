ed5430540dabe7cd467dfe468f85eedf192c5091
Tobb-s <Tobybaziti@gmail.com>
2025-06-29 23:54:06 -0300

    Git Add

---
+++ scripts/04_Presentacion/04_Presentacion.R
@@ -0,0 +1,11 @@
+
+#---------------------------------------------------------Preparacion de Entorno----------------------------------------------------------
+
+install.packages(c(
+  "rmarkdown",    # R Markdown + knitr
+  "knitr",        # Motor de ejecución de chunks
+  "xaringan",     # Presentaciones HTML
+  "flexdashboard",# Dashboards
+  "officer",      # PPTX programático (opcional)
+  "shiny"         # Aplicaciones reactivas
+))
